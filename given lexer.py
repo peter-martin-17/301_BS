99ef60d4c9e98ad846248376e15ddff05bb95177
millett-nick <79597328+millett-nick@users.noreply.github.com>
2022-04-03 23:36:53 -0400

    Update given lexer.py

--- given lexer.py
+++ given lexer.py
@@ -15,10 +15,7 @@ class ClifLexer():
 
 	reserved_bool = {
 		'and': 'AND',
-		'or': 'OR',
-		'if': 'IF',
-		'iff': 'IFF',
-		'not': 'NOT'
+		'or': 'OR'
 	}
 
 
@@ -45,11 +42,7 @@ class ClifLexer():
 		r'\d'
 		return t
 	
-	def t_CHAR(self, t):
 
-		r'[\w?~!#$%^&*_+{}|=:<>\|,./\'\[\]\;\-]'
-		r'[\w]'
-		return t
 	
 	
 	def t_RESERVEDELEMENT(self, t):
@@ -59,28 +52,21 @@ class ClifLexer():
 		r'\w+'
 		if t.value in self.reserved_bool:
 			t.type = self.reserved_bool[t.value]
-			print("Boolean reserved word: " + t.value)
+			#print("Boolean reserved word: " + t.value)
 			return t
 		else:
 			pass
 
-	def t_STRINGQUOTE(self, t):
+	def t_QUOTEDSTRING(self, t):
+		# This is not yet correct: you need to complete the lexing of quotedstring
 		r'\''
 		return t
 
-	def t_NAMEQUOTE(self,t):
-		r'\"'
+	def t_CHAR(self, t):
+
+		r'[\w?~!#$%^&*_+{}|=:<>\|,./\'\[\]\;\-]'
+		r'[\w]'
 		return t
-
-	'''def t_QUOTEDSTRING(self, t):
-		# This is not yet correct: you need to complete the lexing of quotedstring
-		#r '[\self.t_STRINGQUOTE + '(' + self.t_CHAR + '|' self.t_NAMEQUOTE + ')' + self.t_STRINGQUOTE]'
-
-		if (t.value[0] == "'" and t.value[t.value.len-1] == "'"):
-			return t
-
-		else:
-			pass'''
 
 	def lex(self, input_string):
 		self.lexer.input(input_string)
@@ -96,11 +82,11 @@ s = "(and ('max' 1 2 15) (or  ('Func' 'D')))"
 print('Lexing '+s)
 lex.lex(s)
 
-s = "(not ('B' 'C') (if ('C' 'D'))))"
+s = "(and ('B' 'C') (or ('C' 'D'))))"
 print('\nLexing '+s)
 lex.lex(s)
 
 # the following is currently not working but should be accepted because ? is in the set char
-s = "('who' ('is' '?') )"
+s = "('who' ('is' -'?') )"
 print('\nLexing '+s)
 lex.lex(s)
