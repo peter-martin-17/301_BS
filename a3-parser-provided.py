026e2fe25f3fb4eeba3225ec64feeec845230874
millett-nick <79597328+millett-nick@users.noreply.github.com>
2022-04-11 20:47:27 -0400

    Update a3-parser-provided.py

--- a3-parser-provided.py
+++ a3-parser-provided.py
@@ -1,7 +1,14 @@
 import ply.lex as lex
 import ply.yacc as yacc
+import re
+import argparse
 
-class ClifLexer(object):
+#global variable that stores every distinct quoted string each line of the file
+pastQuotedStrings = list()
+
+class ClifLexer():
+	ops_count=0
+	names_count=0
 
 	# CONSTRUCTOR
 	def __init__(self):
@@ -16,18 +23,32 @@ class ClifLexer(object):
 
 	reserved_bool = {
 		'and': 'AND',
-		'or': 'OR'
+		'or': 'OR',
+		'not': 'NOT'
 	}
 
-	tokens = ['OPEN', 'CLOSE', 'QUOTEDSTRING', 'RESERVEDELEMENT']
+	reserved_if = {
+		'if': 'IF',
+		'iff': 'IFF'
+	}
+
+	reserved_comment = {
+		'cl:comment': 'CL:COMMENT'
+	}
+
+
+	tokens = ['OPEN', 'CLOSE', 'QUOTEDSTRING', 'RESERVEDELEMENT', 'NUMERAL', 'COMMENT']
 
 	tokens += reserved_bool.values()
+	tokens += reserved_if.values()
 
-	t_ignore = '\t\r\n\f\v'
+
+	t_ignore = '\t\r\n\f\v '
 
 	def t_NEWLINE(self,t):
 		r'\n+'
 		t.lexer.lineno += len(t.value)
+
 
 	def t_error(self,t):
 		print("Lexing error: Unknown character \"{}\" at line {}".format(t.value[0], t.lexer.lineno))
@@ -38,23 +59,100 @@ class ClifLexer(object):
 	t_OPEN= '\('
 	t_CLOSE= '\)'
 
+	def t_NUMERAL(self, t):
+		r'\d+'
+		return t
+
+	def t_QUOTEDSTRING(self, t):
+		#Original
+		#r"\'[\w?~!\#$%^&*_+{}|=:\"<>\|,./\[\]\;\-]+\'"
+		#EC 
+		r"\'[\w?~!#$%^&*_+{}\=:<>\|,./\[\]\;\-\|\"\|\']+\'"
+
+		#Casts 't' as a string and stores it in 'currentQuotedString'
+		currentQuotedString = str(t)
+
+		#This will store the quoted string
+		substring = ""
+
+		#This stores the first character after ',' in 'currentQuotedString' Example: LexToken(QUOTEDSTRING,<<<<HERE>>>>"'TODAY=03/26/22'",1,10) 
+		firstChar = ""
+
+		#Stores two possible firstChar depending on if the quoted string has a name quote or not
+		if (str(currentQuotedString[22]) == '\''):
+			firstChar = '\''
+		else:
+			firstChar = '"'
+
+		substring += currentQuotedString[22]
+		
+		for i in range(23, len(currentQuotedString)):
+
+			#Special cases for each quoted string
+			if (firstChar == '\'' and i > 23 and str(currentQuotedString[i]) == '\\'):
+				substring += '\\\'\''
+				break
+			
+			elif (firstChar == '"' and str(currentQuotedString[i]) == '"'):
+				substring += '"'
+				break
+			
+			#else just add to substring
+			else:
+				substring += currentQuotedString[i]
+
+		#if its the first quoted string lexed, just add it and don't check for distinctiveness
+		if (len(pastQuotedStrings) == 0):
+			pastQuotedStrings.append(substring)
+			ClifLexer.names_count+=1
+
+		#Check if the new quoted string is equal to any past quoted strings
+		else:
+			isUnique = True
+			for names in pastQuotedStrings:
+
+				if (str(names) == str(substring)):
+					isUnique = False
+			
+			#if the quoted string is distinct, the names_count is added and append the quoted string to distinct quoted strings list
+			if (isUnique):
+				pastQuotedStrings.append(substring)
+				ClifLexer.names_count+=1
+
+		return t
+	
+
 	def t_RESERVEDELEMENT(self, t):
 		# here we use a regular expression to say what matches this particular token:
 		# any sequence of standard characters of length 1 or greater
 		# but this does not yet cover all reservedelements
-		r'\w+'
+		r'[\:\w]+'
 		if t.value in self.reserved_bool:
 			t.type = self.reserved_bool[t.value]
 			#print("Boolean reserved word: " + t.value)
+			ClifLexer.ops_count=ClifLexer.ops_count+1
 			return t
+
+		elif t.value in self.reserved_if:
+			t.type = self.reserved_if[t.value]
+			#print("Boolean reserved word: " + t.value)
+
+			return t
+
+		elif t.value in self.reserved_comment:
+			t.type = 'COMMENT'
+			#print("Boolean reserved word: " + t.value)
+			return t
+
 		else:
 			pass
 
-	def t_QUOTEDSTRING(self, t):
-		# This is not yet correct: you need to complete the lexing of quotedstring
-		# This only matches a single quotation character
-		r'\''
-		return t
+
+	'''t_NAMEQUOTE = r'[\"]'
+	t_STRINGQUOTE = r'[\']'
+	t_DIGIT = r'[\d]'
+	t_CHAR =  r'[\w?~!#$%^&*_+{}|=:<>\|,./\[\]\;\-]'''
+
 
 	def lex(self, input_string):
 		self.lexer.input(input_string)
@@ -64,39 +162,79 @@ class ClifLexer(object):
 				break
 			print(tok)
 
-
 class ClifParser(object):
 
 	tokens = ClifLexer.tokens
-
+	ops_count=0
+	sentenceType = ""
 	# CONSTRUCTOR
 	def __init__(self):
 		print('Parser constructor called.')
+		
 		self.lexer = ClifLexer()
 		self.parser = yacc.yacc(module=self)
+		tokens = ClifLexer.tokens
 
 
 	def p_starter(self, p):
 		"""
 		starter : sentence
 				| sentence starter
+				|
 		"""
-		print("Starting the parsing process.")
+		#print("Starting the parsing process.")
+		
 		pass
 
 	def p_sentence(self, p):
 		"""
-		sentence : OPEN AND QUOTEDSTRING QUOTEDSTRING CLOSE
+		sentence : atomsent
+				| boolsent
+				| commentsent
 		"""
-		# note that the rule above is INCORRECT: it is just an example of how to specify a rule
-		print("Found a sentence: {} {} {} ".format(p[2], p[3], p[4]))
-		if p[3] == p[4]:
-			no_quotedstrings = 1
-		else:
-			no_quotedstrings = 2
 
-		print("Number of distinct quoted strings: " + str(no_quotedstrings))
+	def p_atomsent(self, p):
+		"""
+		atomsent : OPEN termseq CLOSE
+				| OPEN predicate CLOSE
+		"""
 
+		ClifParser.sentenceType = "Atomic: "
+
+
+	def p_boolsent(self, p):
+		"""
+		boolsent :  OPEN AND starter CLOSE
+				| OPEN OR starter CLOSE
+				|  OPEN IFF sentence sentence CLOSE 
+				|  OPEN IF sentence sentence CLOSE 
+				|  OPEN NOT sentence CLOSE 
+		"""
+
+		ClifParser.sentenceType = "Boolean: "
+		
+	def p_commentsent(self, p):
+		"""
+		commentsent : OPEN COMMENT QUOTEDSTRING sentence CLOSE
+		"""
+
+	def p_termseq(self, p):
+		"""
+		termseq :  interpretedname
+				| interpretedname termseq
+				|
+		"""
+
+	def p_predicate(self, p):
+		"""
+		predicate : interpretedname
+		"""
+
+	def p_interpretedname(self, p):
+		"""
+		interpretedname : NUMERAL 
+				| QUOTEDSTRING
+		"""	
 	def p_error(self, p):
 
 		if p is None:
@@ -108,41 +246,59 @@ class ClifParser(object):
 		# Reading the symbols from the Parser stack
 		stack = [symbol for symbol in self.parser.symstack][1:]
 
-		print("Parsing error; current stack: " + str(stack))
+		print("Parsing error;" + str(error_pos) + " current stack: " + str(stack))
 
 
 	def parse(self, input_string):
 		# initialize the parser
 		#parser = yacc.yacc(module=self)
-
+	
 		self.parser.parse(input_string)
 
-# using only the lexer
-lexer = ClifLexer()
-s = "(and ('B' 'C') (or ('C' 'D'))))"
-print('\nLexing '+s)
-lexer.lex(s)
+def main():
 
-parser = ClifParser()
-s = "(and 'Func')"
-#s = "(and ('max' 1 2 15) (or  ('Func' 'D')))"
-print('\nLexing '+s)
-parser.lexer.lex(s)
-print('\nParsing '+s)
-parser.parse(s)
+	takeIn = argparse.ArgumentParser(
+		description="my arg parser"
+	)
 
-parser = ClifParser()
-s = "(or 'Func')"
-#s = "(and ('max' 1 2 15) (or  ('Func' 'D')))"
-print('\nLexing '+s)
-parser.lexer.lex(s)
-print('\nParsing '+s)
-parser.parse(s)
+	takeIn.add_argument('txtName', help="Text File Name", type=str)
+	takeIn.add_argument('parseOrLex', help="Parse or Lex", type=int)
 
-# the following is currently not working but should be accepted because ? is in the set char
-parser = ClifParser()
-s = "('who' ('is' '?') )"
-print('\nLexing '+s)
-parser.lexer.lex(s)
-print('\nParsing '+s)
-parser.parse(s)
+	args = takeIn.parse_args()
+
+	print(args.parseOrLex)
+
+
+	if (args.parseOrLex):
+		myPars = ClifParser()
+		parser = myPars.parser
+
+		myFile = open(args.txtName,'r')
+		newFile= open("newfile.txt", 'w')
+		lineCount = 0
+		for line in myFile:
+			parser.parse(line)
+			#print(ClifParser.sentenceType + line[:-1]+": ops="+str(ClifLexer.ops_count)+", names="+str(ClifLexer.names_count)+"\n")
+			newFile.write(ClifParser.sentenceType + line[:-1]+": ops="+str(ClifLexer.ops_count)+", names="+str(ClifLexer.names_count)+"\n")
+			ClifLexer.ops_count=0
+			ClifLexer.names_count=0
+			pastQuotedStrings.clear()
+			ClifParser.sentenceType = ""
+			lineCount += 1
+
+		print(str(lineCount)+ " sentences")
+		newFile.write(str(lineCount)+ " sentences")
+
+	elif (not(args.parseOrLex)):
+
+		myFile = open(args.txtName,'r')
+
+		lex = ClifLexer()
+		for line in myFile:
+			print('Lexing '+line)
+			lex.lex(line)
+	newFile.close()
+
+
+
+main()
